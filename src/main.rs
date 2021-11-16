96999576fb5ccd9f71c1cb9c4fc6099bed3c6103
Cat Core <arthur.koalas@free.fr>
2021-11-16 23:39:44 +0100

    start working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::fs::File;
+use std::io::Read;
+use cafebabe::ClassFile;
+
 fn main() {
-    println!("Hello, world!");
+    let mut file = File::open("./zz.class").unwrap();
+    let mut bytes = Vec::new();
+    file.read_to_end(&mut bytes).unwrap();
+    println!("Parsing zz.class ...");
+    match cafebabe::parse_class(&bytes) {
+        Ok(class) => parse_class(class),
+        Err(e) => println!("Error: {}", e),
+    };
+}
+
+fn parse_class(class: ClassFile) {
+    // println!("Successfully parsed {:?}\n{:#?}", class.this_class, class);
+    println!("Class name: {}", class.this_class);
+    println!("Fields: {:#?}", class.fields);
 }
